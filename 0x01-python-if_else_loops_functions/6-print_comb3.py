4dbcd0eb20f26bb3583a589d389204111780d57f
john-otienoh <johnteclaire@gmail.com>
2024-01-16 23:08:13 +0300

    fixing-git

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,5 @@ for i in range(0, 10):
         if i < j:
             print("{}".format(i), end="")
             print("{}".format(j), end="")
-            if i < 8 and i < 9:
+            if i  != 8 and i != 9:
                 print(", ", end="")
