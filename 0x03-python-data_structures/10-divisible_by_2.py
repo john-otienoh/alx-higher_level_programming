76ba6d0e68f53db1faa026d0d6fc9a26ab792b26
john-otienoh <johnteclaire@gmail.com>
2024-01-23 05:12:12 +0300

    q

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,9 @@
 def divisible_by_2(my_list=[]):
     for i in range(0, len(my_list)):
         if int(my_list[i]) % 2 == 0:
-            my_list[i] = True
+            #my_list[i] = True
+            return True
         else:
-            my_list[i] = False
-    return my_list
+            #my_list[i] = False
+            return False
+    #return my_list
