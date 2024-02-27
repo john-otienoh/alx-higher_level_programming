31e2439c4fb2972de7b7a0d1fe3cadebf8a6f57d
john-otienoh <johnteclaire@gmail.com>
2024-02-27 05:42:25 +0300

    0x0C. Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """Represent a rectangle."""
+    """Represent a Rectangle."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a new Rectangle.
@@ -21,11 +21,11 @@ class Rectangle(Base):
             TypeError: If either of x or y is not an int.
             ValueError: If x or y is less than 0.
         """
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
