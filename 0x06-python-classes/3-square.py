bfe2ec49f53d6d1056208f041c168314f1299fde
john-otienoh <johnteclaire@gmail.com>
2024-03-06 13:10:04 +0300

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,21 +4,21 @@
 
 
 class Square:
-    """A class Square
+    """
+    A class Square
     """
     def __init__(self, size=0):
-        """Initialize a new square
-
+        """Initialize a new square.
         Args:
             size(int): size of the square class
-            """
+        """
+
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
-        """returns the current square area"""
-        return (self.__size ** 2)
+        """Return the current area of the square"""
+        return self.__size ** 2
