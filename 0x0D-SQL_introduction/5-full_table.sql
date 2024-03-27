59ca3e6ae26856acd223edf7cb7f5442d656901c
john-otienoh <johnteclaire@gmail.com>
2024-03-26 18:39:45 -0700

    0x0D. SQL - Introduction

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- prints the full description of the table first_table 
+-- from the database hbtn_0c_0 in your MySQL server.
+SELECT * FROM information_schema.columns 
+WHERE table_name = 'first_table' AND table_schema = 'hbtn_0c_0';
