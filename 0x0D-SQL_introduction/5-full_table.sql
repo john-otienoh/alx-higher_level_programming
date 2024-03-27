8dc25565f8e879d68cd29b71576a01589fabd448
john-otienoh <johnteclaire@gmail.com>
2024-03-26 18:56:43 -0700

    0x0D. SQL - Introduction

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,3 @@
 -- prints the full description of the table first_table 
 -- from the database hbtn_0c_0 in your MySQL server.
-SELECT * FROM information_schema.columns 
-WHERE table_name = 'first_table' AND table_schema = 'hbtn_0c_0';
+SHOW CREATE TABLE 'first_table'
--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,5 +1,6 @@
 -- display the number of records with id = 89 in the table 
 -- first_table of the database hbtn_0c_0 in MySQL server
 USE hbtn_0c_0;
-SELECT COUNT(id = 89)
-FROM `first_table`;
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id` = 89;
