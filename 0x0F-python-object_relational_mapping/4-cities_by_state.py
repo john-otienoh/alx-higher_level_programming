3e44453af2a09f5bf79fd1ab7a6b51adf3c01bce
john-otienoh <johnteclaire@gmail.com>
2024-05-01 11:09:11 +0300

    helo

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument
+"""
+script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
 """
 import MySQLdb
 import sys
@@ -14,9 +15,8 @@ if __name__ == "__main__":
             database=sys.argv[3],
             port=3306
         )
-    match = sys.argv[4]
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE %S", (match, ))
+    cursor.execute("""SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id=cities.state_id""")
     results = cursor.fetchall()
     for i in results:
         print(i)
