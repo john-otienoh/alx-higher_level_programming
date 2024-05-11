1281b2449d854d4a853d47eb7604bf7a590766b4
john-otienoh <johnteclaire@gmail.com>
2024-05-11 13:20:47 -0700

    solved one task

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status"""
+
+
+if __name__ == '__main__':
+    import urllib.request
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
