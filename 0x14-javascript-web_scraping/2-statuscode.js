25d55c9cc3fbe422633b962580989a44480507cc
john-otienoh <johnteclaire@gmail.com>
2024-06-05 11:19:14 -0700

    fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const optiom = {
-	url: process.argv[2];,
-	method: 'GET';,
-}
-request(optiom, function (e, response) {
-	console.log(e | 'code: ${response.statuscode}');
+const url = process.argv[2];
+request.get(url, function (response) {
+  console.log('code: ${response.statusCode}');
 });
