918f507b657612e422c2b01da6934324f7eea2ce
john-otienoh <johnteclaire@gmail.com>
2024-06-26 13:05:12 -0700

    erroer

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -11,16 +11,6 @@ class Square(Rectangle):
         Args:
             size (int): The side of the new square
         """""
-        super().__init__(size, size)
         super().integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        """Returns the area of the square"""
-        return self.__size * self.__size
-
-    def __str__(self):
-        """Return the print() and str() representation of a Square."""
-        string = "[" + str(self.__class__.__name__) + "] "
-        string += str(self.__size) + "/" + str(self.__size)
-        return string
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,6 +4,7 @@
 
 def is_same_class(obj, a_class):
     """Checks if an object is exactly an instance of the specified class.
+
     Args:
         obj (any): The object to check.
         a_class (type): The class to match the type of obj to.
